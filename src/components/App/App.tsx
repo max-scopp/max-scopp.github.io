373df8d264009b88ac18376ad6cdc89015e0a355
Max Scopp <me@maxscopp.de>
2023-09-07 22:44:21 +0200

    fix page change no scroll to top

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,16 @@
 import styles from "./App.module.scss";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import { useEffect } from "preact/hooks";
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ left: 0, top: 0, behavior: "instant" });
+  }, [location.pathname]);
+
   return (
     <div className={styles.host}>
       <Header></Header>
